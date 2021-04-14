90e66e6679a56dc94f8f682e7bfb4dcddf3c0966
haikalwahyudi <cocakun459@gmail.com>
2021-04-14 12:26:45 +0800

    templatting

---
+++ app/Views/layout/v_sidebar.php
@@ -0,0 +1,26 @@
+<div class="main-sidebar">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.html">St</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="menu-header">Starter</li>
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+          </ul>
+        </li>
+        <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+          <ul class="dropdown-menu">
+            <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
+          </ul>
+        </li>
+      </ul>
+  </aside>
+</div>
