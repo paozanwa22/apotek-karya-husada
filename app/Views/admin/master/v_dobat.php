02cd5e706de2dec716f8ebea8f4666ee37f0636d
haikalwahyudi <cocakun459@gmail.com>
2021-04-18 14:42:46 +0800

    Data Obat

--- app/Views/admin/master/v_dobat.php
+++ app/Views/admin/master/v_dobat.php
@@ -18,7 +18,33 @@
                     <div class="col col-md-12">
                         <div class="card">
                             <div class="card-body">
-                                <table class="table">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Kode Obat</th>
+                                            <th>Nama Obat</th>
+                                            <th>Satuan</th>
+                                            <th>Kategori</th>
+                                            <th>Harga Beli</th>
+                                            <th>Harga Jual</th>
+                                            <th>Stok</th>
+                                            <th>Tanggal Kadaluarsa</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>a</td>
+                                            <td>a</td>
+                                            <td>a</td>
+                                            <td>a</td>
+                                            <td>a</td>
+                                            <td>a</td>
+                                            <td>a</td>
+                                            <td>a</td>
+                                        </tr>
+                                    </tbody>
                                 </table>
                             </div>
                         </div>
