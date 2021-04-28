ffc7be938ad60e2ad743ab1668f40da286638bd6
haikalwahyudi <cocakun459@gmail.com>
2021-04-28 16:17:39 +0800

    Membuat form login2

--- app/Views/login/v_login.php
+++ app/Views/login/v_login.php
@@ -21,16 +21,18 @@
 <body>
   <div id="app">
     <section class="section">
-      <div class="d-flex flex-wrap align-items-stretch">
-        <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
+      <!-- <div class="d-flex flex-wrap align-items-stretch"> -->
+      <div class="row" style="background-color: #D7FFE3">
+      
+        <div class="col-lg-4 col-md-6 col-12 col-sm-7 order-lg-1 min-vh-100 order-1 bg-white">
           <div class="p-4 m-3">
-            <img src="<?= base_url(); ?>/template/assets/img/stisla-fill.svg" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2">
+            <img src="<?= base_url(); ?>/template/assets/img/logo.png" alt="logo" width="80" class="shadow-light rounded-circle mb-5 mt-2">
             <h4 class="text-dark font-weight-normal">Apotek Karya Husada</h4>
             <p class="text-muted">Silahkan <span class="font-weight-bold">Login</span></p>
-            <form method="POST" action="#">
+            <form method="POST" action="#" class="needs-validation" novalidate="">
               <div class="form-group">
                 <label for="email">Username</label>
-                <input type="text" class="form-control" name="username" tabindex="1" required autofocus>
+                <input type="text" class="form-control" name="username" tabindex="1" autofocus>
               </div>
 
               <div class="form-group">
@@ -44,7 +46,7 @@
                 <a href="auth-forgot-password.html" class="float-left mt-3">
                   Lupa Password?
                 </a>
-                <button type="submit" class="btn btn-primary btn-lg btn-icon icon-right" tabindex="4">
+                <button type="submit" class="btn btn-success btn-lg btn-icon icon-right" tabindex="4">
                   Masuk
                 </button>
               </div>
@@ -53,18 +55,13 @@
 
           </div>
         </div>
-        <div class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom" data-background="<?= base_url() ?>/template/assets/img/unsplash/login-bg.jpg">
-          <div class="absolute-bottom-left index-2">
-            <div class="text-light p-5 pb-2">
-              <div class="mb-5 pb-3">
-                <h1 class="mb-2 display-4 font-weight-bold">Good Morning</h1>
-                <h5 class="font-weight-normal text-muted-transparent">Bali, Indonesia</h5>
-              </div>
-              Photo by <a class="text-light bb" target="_blank" href="https://unsplash.com/photos/a8lTjWJJgLA">Justin Kauffman</a> on <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
-            </div>
-          </div>
+        <div class="col-lg-8 col-md-6 col-sm-5 order-1 text-center" style="margin-top: 120px;">
+            <h4>Apotek Karya Husada Farma</h4>
+            <img src="<?= base_url(); ?>/template/assets/img/login.png" width="500px" alt="Apotek">
         </div>
+      
       </div>
+      <!-- </div> -->
     </section>
   </div>
 
